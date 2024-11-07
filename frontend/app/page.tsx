88251c8daa21047bcb55c802cea29ddf53b55614
Alex Smith <xelanybor@gmail.com>
2024-11-07 21:58:35 +0200

    added floating menu button

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,17 +6,28 @@ import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 function Map() {
   return (
-    <TransformWrapper>
-      <TransformComponent>
-        <img src="/maps/Oasis2.png" alt="test" />
-      </TransformComponent>
-    </TransformWrapper>
+    <div className="map-container">
+      <TransformWrapper>
+        <TransformComponent>
+          <img src="/maps/Oasis2.png" alt="map" />
+        </TransformComponent>
+      </TransformWrapper>
+    </div>
+  );
+}
+
+function FloatingButton() {
+  return (
+    <div className="floating-button">
+      <Image src="/icons/burger.svg" alt="button" width="100" height="100" />
+    </div>
   );
 }
 
 export default function Home() {
   return (
     <>
+      <FloatingButton />
       <Map />
     </>
   );
