af36114ab0bc97ecb78c93675a1bf3963abef95b
Alex Smith <xelanybor@gmail.com>
2024-11-08 22:36:54 +0200

    added mantine

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,6 +4,11 @@ import Image from "next/image";
 import React, { Component } from "react";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
+import { Burger, MantineProvider, Pill } from "@mantine/core"
+
+import TeamManager from "../components/teamManager";
+import { useDisclosure } from "@mantine/hooks";
+
 function Map() {
   return (
     <div className="map-container">
@@ -30,16 +35,19 @@ function FloatingButton({ buttonType, side = "left", position = 0 }: { buttonTyp
     // set the vertical position of the button
     style["top"] = `${1 + 5 * position}rem`;
 
+    const [opened, { toggle }] = useDisclosure();
+
   return (
     <div className="floating-button"  style={style}>
-      <Image src={`/icons/${buttonType}.svg`} alt="button" width="100" height="100" />
+      {/* <Image src={`/icons/${buttonType}.svg`} alt="button" width="100" height="100" /> */}
+      <Burger opened={opened} onClick={toggle} aria-label="Toggle navigation" />
     </div>
   );
 }
 
 export default function Home() {
   return (
-    <>
+    <MantineProvider>
       <FloatingButton buttonType="burger" side="left" position={0} />
       <FloatingButton buttonType="export" side="left" position={1} />
       <FloatingButton buttonType="pencil" side="left" position={2} />
@@ -47,7 +55,10 @@ export default function Home() {
       <FloatingButton buttonType="map" side="right" position={0} />
       <FloatingButton buttonType="layers" side="right" position={1} />
       <FloatingButton buttonType="point" side="right" position={2} />
+
+      {/* <TeamManager /> */}
+
       <Map />
-    </>
+    </MantineProvider>
   );
 }
