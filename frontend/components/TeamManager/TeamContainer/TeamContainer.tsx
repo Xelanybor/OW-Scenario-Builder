1cd04cc32ceeaf9b5898a441c3e44b942cd53d14
Alex Smith <xelanybor@gmail.com>
2025-03-05 13:28:11 +0200

    change hero spacing in player manager

--- frontend/components/TeamManager/TeamContainer/TeamContainer.tsx
+++ frontend/components/TeamManager/TeamContainer/TeamContainer.tsx
@@ -10,7 +10,7 @@ import PlayerManager from './PlayerManager/PlayerManager';
 
 export default function TeamContainer({color}: {color: React.CSSProperties['color']}) {
   return (
-    <Group classNames={{root: classes.teamContainer}} justify='center'>
+    <Group classNames={{root: classes.teamContainer}} justify='center' gap='1vw'>
       <PlayerManager />
       <PlayerManager />
       <PlayerManager />
