63772aa20f7dabd60aa1719b424db38f65956bc5
Alex Smith <xelanybor@gmail.com>
2025-03-06 08:29:03 +0200

    implement discord sign in

--- frontend/components/SidebarMenu/SidebarMenu.tsx
+++ frontend/components/SidebarMenu/SidebarMenu.tsx
@@ -2,13 +2,15 @@
 
 import React from "react";
 
+import SignInButton from "@/components/Buttons/SignInButton/SignInButton";
+
 import classes from './SidebarMenu.module.css'
 
 export default function SidebarMenu() {
 
     return (
         <div className={classes.sidebarMenu}>
-            stuff goes here
+            <SignInButton />
         </div>
     );
 }
