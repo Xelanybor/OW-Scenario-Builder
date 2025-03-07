a8564273c064f41e7232007898f54cbf84a0cb7d
Alex Smith <xelanybor@gmail.com>
2025-03-07 09:23:39 +0200

    refactor main page to a server component

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,51 +1,13 @@
-'use client'
-
 import React from 'react';
+import { SessionProvider, useSession } from 'next-auth/react';
 
-import { useDisclosure } from '@mantine/hooks';
-import { Drawer } from '@mantine/core';
-
-import FloatingContainer from '../components/FloatingContainer/FloatingContainer'
-import Map from '../components/Map/Map'
-import SidebarMenu from '@/components/SidebarMenu/SidebarMenu';
-import IconButton from '@/components/Buttons/IconButton/IconButton';
-import { SessionProvider } from 'next-auth/react';
+import ScenarioEditor from '@/components/ScenarioEditor/ScenarioEditor';
 
 export default function HomePage() {
 
-  const [opened, {open, close}] = useDisclosure(false);
-
   return (
     <SessionProvider>
-      <FloatingContainer side="left" position={0}>
-        <IconButton buttonType="burger" onClick={open} />
-      </FloatingContainer>
-
-      <FloatingContainer side="left" position={1}>
-        <IconButton buttonType="export" />
-      </FloatingContainer>
-
-      <FloatingContainer side="left" position={2}>
-        <IconButton  buttonType="pencil" />
-      </FloatingContainer>
-
-      <FloatingContainer side="right" position={0}>
-        <IconButton buttonType="map" />
-      </FloatingContainer>
-
-      <FloatingContainer side="right" position={1}>
-        <IconButton buttonType="layers" />
-      </FloatingContainer>
-      
-      <FloatingContainer side="right" position={2}>
-        <IconButton buttonType="point" />
-      </FloatingContainer>
-
-      <Drawer opened={opened} onClose={close} padding="md" title="OW Scenario Builder">
-        <SidebarMenu />
-      </Drawer>
-
-      <Map />
+      <ScenarioEditor />
     </SessionProvider>
   );
 }
