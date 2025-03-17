ed55713ab301912766e6b30aa8862c4801e07a18
Alex Smith <xelanybor@gmail.com>
2025-03-17 12:25:24 +0200

    readd team manager to main page after refactor

--- frontend/components/ScenarioEditor/ScenarioEditor.tsx
+++ frontend/components/ScenarioEditor/ScenarioEditor.tsx
@@ -6,6 +6,8 @@ import { Drawer } from '@mantine/core';
 
 import { useDisclosure } from '@mantine/hooks';
 
+import TeamManager from '@/components/TeamManager/TeamManager';
+import TeamManagerButton from '@/components/Buttons/TeamManagerButton/TeamManagerButton';
 import FloatingContainer from '@/components/FloatingContainer/FloatingContainer'
 import Map from '@/components/Map/Map'
 import SidebarMenu from '@/components/SidebarMenu/SidebarMenu';
@@ -13,12 +15,13 @@ import IconButton from '@/components/Buttons/IconButton/IconButton';
 
 export default function ScenarioEditor() {
 
-    const [opened, {open, close}] = useDisclosure(false);
+  const [menuOpened, {open: openMenu, close: closeMenu}] = useDisclosure(false); // side menu state
+    const [teamManagerOpened, {open: openTeamManager, close: closeTeamManager, toggle: toggleTeamManager}] = useDisclosure(false); // team manager state
 
     return (
         <div>
           <FloatingContainer side="left" position={0}>
-            <IconButton buttonType="burger" onClick={open} />
+            <IconButton buttonType="burger" onClick={openMenu} />
           </FloatingContainer>
 
           <FloatingContainer side="left" position={1}>
@@ -41,8 +44,14 @@ export default function ScenarioEditor() {
             <IconButton buttonType="point" />
           </FloatingContainer>
 
-          <Drawer opened={opened} onClose={close} padding="md" title="OW Scenario Builder">
+          <Drawer opened={menuOpened} onClose={closeMenu} padding="md" title="OW Scenario Builder">
             <SidebarMenu />
+          </Drawer>
+
+          <TeamManagerButton style={{position: 'fixed', zIndex: '1', bottom: 0, left: '50%', transform: 'translateX(-50%)', opacity: teamManagerOpened ? 0 : 1}} toggleDrawer={toggleTeamManager} drawerOpen={teamManagerOpened} />
+
+          <Drawer styles={{content: {overflow: 'visible'}}} position="bottom" opened={teamManagerOpened} transitionProps={{transition: 'slide-up'}} onClose={closeTeamManager} padding="md" size="11vw" withCloseButton={false}>
+            <TeamManager toggleDrawer={toggleTeamManager} drawerOpen={teamManagerOpened} />
           </Drawer>
 
           <Map />
