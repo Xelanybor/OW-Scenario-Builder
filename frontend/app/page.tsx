5386248a7df9fcc621f72d83b5b0af378cad8ecd
Alex Smith <xelanybor@gmail.com>
2025-03-26 10:14:41 +0200

    remove unnecessary import
    I love committing straight to main

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,7 +3,6 @@ import { SessionProvider, useSession } from 'next-auth/react';
 import { ModalsProvider } from '@mantine/modals';
 
 import ScenarioEditor from '@/components/ScenarioEditor/ScenarioEditor';
-import { ModalProvider } from '@mantine/core/lib/components/Modal/Modal.context';
 
 export default function HomePage() {
 
