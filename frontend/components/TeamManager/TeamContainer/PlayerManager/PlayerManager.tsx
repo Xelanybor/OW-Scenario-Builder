75afc3f14b00a05e9de456c157175df27ee13b3e
Alex Smith <xelanybor@gmail.com>
2025-03-26 10:11:22 +0200

    remove unnecessary print statement

--- frontend/components/TeamManager/TeamContainer/PlayerManager/PlayerManager.tsx
+++ frontend/components/TeamManager/TeamContainer/PlayerManager/PlayerManager.tsx
@@ -72,8 +72,6 @@ export default function PlayerManager({ teamID, playerID, teamColour, scenarioSt
     }
   }
 
-  console.log(scenario);
-
   return (
     <>
       <div className={classes.playerContainer} onClick={() => {
