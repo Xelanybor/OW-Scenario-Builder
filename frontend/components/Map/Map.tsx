05dc66ca309287ad6a3f18ac802b566662808cb2
Alex Smith <xelanybor@gmail.com>
2025-03-27 10:57:55 +0200

    add map images for OW1 escort and hybrid maps

--- frontend/components/Map/Map.tsx
+++ frontend/components/Map/Map.tsx
@@ -7,6 +7,7 @@ import classes from './Map.module.css'
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import { State } from "@/types/Util";
 import { Scenario } from "@/types/Scenario";
+import { GamemodeAvailableMaps } from "@/types/Maps";
 
 import { getMapImageName } from "@/utils/filenames";
 
@@ -16,13 +17,18 @@ export default function Map({ scenarioState } : { scenarioState: State<Scenario>
 
   let style: React.CSSProperties = {};
 
-    return (
-      <div className={classes.mapContainer} style={style}>
-        <TransformWrapper initialScale={1}>
-          <TransformComponent>
-            <img src={`/maps/${getMapImageName(scenario.map)}${scenario.point}.png`} alt="map" />
-          </TransformComponent>
-        </TransformWrapper>
-      </div>
-    );
-  }
+  const point = GamemodeAvailableMaps['Control'].includes(scenario.map) ? `${scenario.point}` : '';
+
+  const mapFileName = `/maps/${getMapImageName(scenario.map)}${point}.png`;
+
+
+  return (
+    <div className={classes.mapContainer} style={style}>
+      <TransformWrapper initialScale={1}>
+        <TransformComponent>
+          <img src={mapFileName} alt="map" />
+        </TransformComponent>
+      </TransformWrapper>
+    </div>
+  );
+}
